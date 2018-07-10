b0f52e65b67f644dc7c71c16db9707f6184d74f8
Sumit Sarkar <ssarkar@cimpress.com>
2018-07-10 11:16:22 +0530

    FIX: Egress rule

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "aws_security_group" "container_instance" {
 
   egress {
     from_port = 0
-    to_port = 0
+    to_port = 65535
     protocol = "-1"
     cidr_blocks = [
       "0.0.0.0/0"]
